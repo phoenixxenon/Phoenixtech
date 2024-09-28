ec9413cc93b413b0bc1769786ef7252fbc54af24
Hubdarkweb <klenin980@gmail.com>
2024-09-28 05:38:58 +0300

    Update bbr.sh

--- ubuntu/bbr.sh
+++ ubuntu/bbr.sh
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing 𝐓𝐂𝐏 𝐁𝐁𝐑 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐂𝐇𝐀𝐏𝐄𝐄𝐘 𝐒𝐓𝐎𝐑𝐄"
+echo -e "Installing 𝐓𝐂𝐏 HE3ndrixx  𝐒𝐓𝐎𝐑𝐄"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
